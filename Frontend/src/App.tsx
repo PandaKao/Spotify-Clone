c5df48b64f7c77a752b18d2718ea84129e3f5416
Justin Kao <justinkao010@gmail.com>
2025-02-25 12:29:53 -0800

    initialized app

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-500 text-5xl">hello</h1>
+      <Button variant={"outline"}>This is a button</Button>
+    </>
+  )
+}
+
+export default App
