d30785b141bd5dd3b1929b6d793851cbe37abd1f
Justin Kao <justinkao010@gmail.com>
2025-04-10 11:14:27 -0700

    added song routes getAllSongs, getFeaturedSongs, getTopSongs

---
+++ Backend/src/controller/songController.js
@@ -0,0 +1,79 @@
+import { Song } from "../models/songModel";
+
+export const getAllSongs = async (req, res, next) => {
+    try {
+        const songs = await Song.find().sort({ createdAt: -1 });
+        res.json(songs);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getFeaturedSongs = async (req, res, next) => {
+    try {
+        // Fetch random songs using MoongoDb aggregation
+        const songs = await Song.aggregate([
+            {
+                $sample: { size: 6 }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            }
+        ]);
+
+        res.json(songs);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getTopSongs = async (req, res, next) => {
+    try {
+        const userId = req.user._id;
+        const userTopSongs = await Song.aggregate([
+            {
+                $match: {
+                    "plays.userId": userId
+                }
+            },
+            {
+                $addFields: {
+                    playCount: {
+                        $size: {
+                            $filter: {
+                                input: '$plays',
+                                as: 'play',
+                                cond: { $eq: ['$$play.userId', userId] }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $sort: { playCount: -1 }
+            },
+            {
+                $limit: 4
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            }
+        ]);
+
+        res.json(userTopSongs);
+    } catch (error) {
+        next(error);
+    }
+};
--- Backend/src/routes/songRoute.js
+++ Backend/src/routes/songRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import {getAllSongs, getFeaturedSongs, getTopSongs, getTrendingSongs} from "../controller/songController";
+import {protectRoute, requireAdmin} from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send("Song route with GET method");
-});
+router.get('/', protectRoute, requireAdmin, getAllSongs);
+router.get('/featured', getFeaturedSongs);
+router.get('/made-for-you', getTopSongs);
 
 export default router;
