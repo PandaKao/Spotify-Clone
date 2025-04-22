eaa82374eec12f9edbb125c8ce282bf3f5d8cdf8
Justin Kao <justinkao010@gmail.com>
2025-04-22 16:59:36 -0700

    added framework for main layout

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
-import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/Mainlayout";
 
 
 function App() {
@@ -9,9 +10,12 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
         <Route path='/sso-callback' element={<AuthenticateWithRedirectCallback signUpFallbackRedirectUrl={'/auth-callback'} />} />
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<HomePage />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ Frontend/src/layout/Mainlayout.tsx
@@ -0,0 +1,30 @@
+import {Outlet} from "react-router-dom";
+import {ResizablePanel, ResizablePanelGroup, ResizableHandle} from "@/components/ui/resizable";
+
+const MainLayout = () => {
+    const isMobile = false;
+    return <div className="h-screen bg-black text-white flex flex-col">
+        <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
+            {/* left sidebar */}
+            <ResizablePanel defaultSize={20} minSize={isMobile ? 0:10} maxSize={30}>
+                left sidebar
+            </ResizablePanel>
+
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+            {/* main content */}
+            <ResizablePanel defaultSize={isMobile ? 80:60}>
+                <Outlet />
+            </ResizablePanel>
+
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+            {/* right sidebar */}
+            <ResizablePanel defaultSize={20} minSize={isMobile ? 0:10} maxSize={25} collapsedSize={0}>
+                right sidebar
+            </ResizablePanel>
+        </ResizablePanelGroup>
+
+    </div>
+};
+export default MainLayout;
